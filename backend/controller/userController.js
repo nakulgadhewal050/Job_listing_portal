565651225786dac7cc43cb5e0397f61ac1e7ddc8
nakul gadhewal <nakulgadhewal050@gmail.com>
2025-11-19 19:24:55 +0530

    sone changes

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -13,7 +13,6 @@ export const getCurrentUser = async (req, res) => {
         if(!user){
             return res.status(404).json({message: "User not found"});
         }
-        console.log("user found successfully");
         return res.status(200).json(user);
     } catch (error) {
          return res.status(500).json({message: ` getCurrentUser error: ${error}`});
