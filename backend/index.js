bd880fa8d522286bbf137b0614cb06de7390e0b0
nakul gadhewal <nakulgadhewal050@gmail.com>
2025-11-28 16:07:21 +0530

    ad

--- backend/index.js
+++ backend/index.js
@@ -20,10 +20,18 @@ app.use(cookieParser());
 
 // CORS configuration for production
 app.use(cors({
-    origin: [
-        process.env.FRONTEND_URL,
-        'http://localhost:5173'
-    ],
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            process.env.FRONTEND_URL,
+            'https://job-listing-portal-frontend-9tvf.onrender.com',
+            'http://localhost:5173'
+        ];
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
